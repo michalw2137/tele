eddca5febb271766938f3faa9c6f14856c8d3661
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-04-08 16:48:15 +0200

    creating headers and counting checksum

--- sender/src/main.cpp
+++ sender/src/main.cpp
@@ -7,11 +7,62 @@
 #include "../include/codes.h"
 #include <math.h>       /* floor */
 
+int maxBlocks;
+
 void fillHeader(byte header[3], byte i) {
     header[0] = SOH;
     header[1] = i;
     header[2] = 255-i;
 }
+
+void splitIntoBlocks(std::vector<byte> buffer, byte blocks [][128] ) {
+    int fullBlocks = (int) floor(buffer.size()/128.);
+
+    // pełne bloki
+    byte i=0;
+    while (i < fullBlocks) {
+        for (int ii=0; ii<128; ii++) {
+            blocks[i][ii] = buffer[128*i + ii];
+        }
+        i++;
+    }
+
+    // niepełny blok
+    byte bytesLeft = buffer.size() - (i * 128);
+    if (bytesLeft > 0) {
+        //wpisanie pozostalych bajtow
+        int ii = 0;
+        while (ii < bytesLeft) {
+            blocks[i][ii] = buffer[128*i + ii];
+            ii++;
+        }
+        //dopisanie zer
+        while (ii < 128) {
+            blocks[i][ii] = 'x';
+            ii++;
+        }
+        i++;
+    }
+}
+
+void printBlocks(byte blocks [][128]) {
+    for (int i = 0; i<maxBlocks; i++) {
+        for (int ii=0; ii<128; ii++) {
+            std::cout << blocks[i][ii];
+        }
+        std::cout << '\n';
+    }
+}
+
+byte algebraicChecksum(byte bytes[128]){
+    byte sum = 0;
+    for(int i=0; i<128; i++){
+        sum += bytes[i];
+        sum %= 256;
+    }
+    return sum;
+}
+
 
 int main() {
     bool isSuccesful;
@@ -48,65 +99,50 @@ int main() {
     timeouts.WriteTotalTimeoutMultiplier = 10;
 
     std::string fileName = "message.txt";
-//    std::cout << "File to send: ";
-//    getline(std::cin, fileName);
     std::ifstream input("../files/" + fileName, std::ios::binary);
-    // copies all data into buffer
     std::vector<byte> buffer(std::istreambuf_iterator<char>(input), {});
+    maxBlocks = ceil(buffer.size()/128.);
+
+    byte blocks[maxBlocks][128];
+
+    splitIntoBlocks(buffer, blocks);
+//    printBlocks(blocks);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     DWORD bytesWritten;
     DWORD bytesRead;
 
-    for (byte b: buffer)
-        std::cout << b;
-    std::cout << '\n';
 
-//    byte i = 0;
-    byte header[3];
+    int blockNumber = 0;
+    while (blockNumber < maxBlocks) {
+        byte block[128];
+        for(int i=0; i<128; i++) {
+            block[i] = blocks[blockNumber][i];
+        }
+
+        byte header[3];
+        fillHeader(header, blockNumber);
+
+        std::cout << "BLOK nr " << blockNumber <<'\n';
+        std::cout << "header: \n";
+        std::cout << (int)header[0] << "_" << (int)header[1] << "_" << (int)header[2] << "\n";
+
+        std::cout << "blok danych: \n";
+        for(int i=0; i<128; i++) {
+            std::cout << block[i];
+        } std::cout << '\n';
+
+        std::cout << "suma kontrolna: \n";
+        std::cout << (int)algebraicChecksum(block) << "\n\n";
+
+        blockNumber ++;
+    }
 
     // wysłanie nagłówka
 //    fillHeader(header, i);
 //    isSuccesful = WriteFile(port, header, 3, &bytesWritten, NULL);
 
-    // wysłanie danych
-    // pełne bloki
-    byte i=0;
-    while (i < floor(buffer.size() / 128)) {
-        byte data[128];
-        for (int ii=0; ii<128; ii++) {
-            data[ii] = buffer[128*i + ii];
-        }
-        std::cout << (int) i << " block: \n";
-        for (byte ii : data) {
-            std::cout << ii;
-        }
-        std::cout << "\n";
-        i++;
-    }
-    // niepełny blok
-    byte bytesLeft = buffer.size() - (i * 128);
-    if (bytesLeft > 0) {
-        std::cout << "\nNIEPELNY BLOK \n";
-        byte data[128];
-        int ii = 0;
-        //wpisanie pozostalych bajtow
-        while (ii < bytesLeft) {
-            data[ii] = buffer[128*i + ii];
-            ii++;
-        }
-        //dopisanie zer
-        while (ii < 128) {
-            data[ii] = 'x';
-            ii++;
-        }
 
-        std::cout << (int) i << " block: \n";
-        for (byte ii : data) {
-            std::cout << ii;
-        }
-        std::cout << "\n";
-        i++;
-    }
 //    isSuccesful = WriteFile(port, data, 128, &bytesWritten, NULL);
 
     //wysłanie sumy kotrolnej
