45b17a3ef0b511de6502f063b352c8bae9b91fb6
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-04-08 16:02:44 +0200

    spliting data into blocks, filling last one with zeroes

--- sender/src/main.cpp
+++ sender/src/main.cpp
@@ -2,18 +2,29 @@
 #include <windows.h>
 #include <vector>
 #include <fstream>
+#include <cstring>
 #include "../include/typedef.h"
 #include "../include/codes.h"
+#include <math.h>       /* floor */
+
+void fillHeader(byte header[3], byte i) {
+    header[0] = SOH;
+    header[1] = i;
+    header[2] = 255-i;
+}
 
 int main() {
+    bool isSuccesful;
+    int SUM_SIZE = 1;
+
     HANDLE port;
     port = CreateFileA(R"(\\.\COM21)",                //port name
-                        GENERIC_READ | GENERIC_WRITE, //Read/Write
-                        0,                            // No Sharing
-                        NULL,                         // No Security
-                        OPEN_EXISTING,// Open existing port only
-                        0,            // Non Overlapped I/O
-                        NULL);        // Null for Comm Devices
+                       GENERIC_READ | GENERIC_WRITE, //Read/Write
+                       0,                            // No Sharing
+                       NULL,                         // No Security
+                       OPEN_EXISTING,// Open existing port only
+                       0,            // Non Overlapped I/O
+                       NULL);        // Null for Comm Devices
 
     if (port == INVALID_HANDLE_VALUE)
         throw std::runtime_error("failed to open port");
@@ -30,42 +41,81 @@ int main() {
 
     //konfiguracja timeouts
     COMMTIMEOUTS timeouts = {0};
-    timeouts.ReadIntervalTimeout = 5000;		//in ms
+    timeouts.ReadIntervalTimeout = 5000;        //in ms
     timeouts.ReadTotalTimeoutConstant = 5000;
     timeouts.ReadTotalTimeoutMultiplier = 10;
     timeouts.WriteTotalTimeoutConstant = 50;
     timeouts.WriteTotalTimeoutMultiplier = 10;
 
-    std::string fileName = "jpg.jpg";
+    std::string fileName = "message.txt";
 //    std::cout << "File to send: ";
 //    getline(std::cin, fileName);
-    std::ifstream input( "../files/" + fileName, std::ios::binary );
+    std::ifstream input("../files/" + fileName, std::ios::binary);
     // copies all data into buffer
     std::vector<byte> buffer(std::istreambuf_iterator<char>(input), {});
 
     DWORD bytesWritten;
     DWORD bytesRead;
 
-    for(byte b: buffer)
+    for (byte b: buffer)
         std::cout << b;
     std::cout << '\n';
 
-//    while(1)
-    WriteFile(port, buffer.data(), buffer.size(), &bytesWritten, NULL);
+//    byte i = 0;
+    byte header[3];
 
-    byte answer;
-    std::cout << "Expected: NAK = " << NAK << '\n';
-    while (true) {
-        ReadFile(port, &answer, sizeof(answer), &bytesRead, NULL);
+    // wysłanie nagłówka
+//    fillHeader(header, i);
+//    isSuccesful = WriteFile(port, header, 3, &bytesWritten, NULL);
 
-        if (bytesRead == 0) {
-            std::cout << "no bytes read" << '\n';
-        } else {
-            std::cout << " BYTES READ: " << bytesRead <<'\n';
-            std::cout << answer << '\n';
+    // wysłanie danych
+    // pełne bloki
+    byte i=0;
+    while (i < floor(buffer.size() / 128)) {
+        byte data[128];
+        for (int ii=0; ii<128; ii++) {
+            data[ii] = buffer[128*i + ii];
+        }
+        std::cout << (int) i << " block: \n";
+        for (byte ii : data) {
+            std::cout << ii;
+        }
+        std::cout << "\n";
+        i++;
+    }
+    // niepełny blok
+    byte bytesLeft = buffer.size() - (i * 128);
+    if (bytesLeft > 0) {
+        std::cout << "\nNIEPELNY BLOK \n";
+        byte data[128];
+        int ii = 0;
+        //wpisanie pozostalych bajtow
+        while (ii < bytesLeft) {
+            data[ii] = buffer[128*i + ii];
+            ii++;
+        }
+        //dopisanie zer
+        while (ii < 128) {
+            data[ii] = 'x';
+            ii++;
         }
 
+        std::cout << (int) i << " block: \n";
+        for (byte ii : data) {
+            std::cout << ii;
+        }
+        std::cout << "\n";
+        i++;
     }
+//    isSuccesful = WriteFile(port, data, 128, &bytesWritten, NULL);
+
+    //wysłanie sumy kotrolnej
+    byte sum[SUM_SIZE];
+
+//    isSuccesful = WriteFile(port, sum, SUM_SIZE, &bytesWritten, NULL);
+
+
+
 
 
     CloseHandle(port);
