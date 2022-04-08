1e7999f4d225dad5aef45ecd3f380bbeee28feb0
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-04-08 12:16:31 +0200

    sending works

---
+++ sender/include/codes.h
@@ -0,0 +1,16 @@
+//
+// Created by micha on 08.04.2022.
+//
+
+#ifndef SENDER_CODES_H
+#define SENDER_CODES_H
+
+const byte SOH = 0x01;
+const byte EOT = 0x04;
+const byte ACK = 0x06;
+const byte NAK = 0x15;
+const byte CAN = 0x18;
+const byte C   = 0x43;
+
+
+#endif //SENDER_CODES_H
